72e5cd2c3b579f53fa8fd22e875b0bc91556d353
Dech100 <79260403+Dech100@users.noreply.github.com>
2021-11-07 19:58:25 -0300

    Update Setup_Database.sql

--- data/Setup_Database.sql
+++ data/Setup_Database.sql
@@ -94,12 +94,21 @@ INSERT INTO Subject (code,name,description,creationTime) VALUES
 SELECT * FROM Subject;
 
 INSERT INTO Review (feedback,score,creationTime,reviewerId) VALUES
-('odeio muito tudo isso, como esse professor quer que eu estude? Quem ele pensa que ele é? Tem como dar nota negativa nesse coisa?',1,NOW(),SELECT id FROM `User` WHERE username LIKE 'chorao'),
-('Começou ruim, mas hoje é uma das melhores aulas',8,NOW(),SELECT id FROM `User` WHERE username LIKE 'rangeru'),
-('Capacitor goes Boom',7,NOW(),SELECT id FROM `User` WHERE username LIKE 'kalc'),
-('MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA',1,NOW(),SELECT id FROM `User` WHERE username LIKE 'DIO'),
+('odeio muito tudo isso, como esse professor quer que eu estude? Quem ele pensa que ele é? Tem como dar nota negativa nesse coisa?',1,NOW(),(SELECT id FROM `User` WHERE username LIKE 'chorao')),
+('Começou ruim, mas hoje é uma das melhores aulas',8,NOW(),(SELECT id FROM `User` WHERE username LIKE 'rangeru')),
+('Capacitor goes Boom',7,NOW(),(SELECT id FROM `User` WHERE username LIKE 'kalc')),
+('MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA MUDA',1,NOW(),(SELECT id FROM `User` WHERE username LIKE 'DIO'))
+('Esse maluco chora de tudo, pela mor',3,NOW(),(SELECT id FROM `User` WHERE username LIKE 'kalc')),
+('Nunca ví esse maluco',5,NOW(),(SELECT id FROM `User` WHERE username LIKE 'dono')),
+('I SLEEP',3,NOW(),(SELECT id FROM `User` WHERE username LIKE 'kalc')),
+('Cocaine is a hell of a drug',8,NOW(),(SELECT id FROM `User` WHERE username LIKE 'kalc'));
 SELECT * FROM Review;
 
 INSERT INTO UserSubjects (userId,subjectId) VALUES (1,1); 
 SELECT * FROM UserSubjects;
 
+INSERT INTO UserReviews (reviewId,reviewedSubjectId) VALUES ();
+SELECT * FROM UserReviews;
+
+INSERT INTO SubjectReviews (reviewId,reviewedSubjectId) VALUES ();
+SELECT * FROM UserReviews;

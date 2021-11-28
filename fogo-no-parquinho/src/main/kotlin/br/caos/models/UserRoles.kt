411b9a2fa3e957abc85375c070f2e813165c9f6e
julyca <jufsantana1@gmail.com>
2021-11-27 22:19:12 -0300

    Adicionando comentários à classe UserRoles

--- fogo-no-parquinho/src/main/kotlin/br/caos/models/UserRoles.kt
+++ fogo-no-parquinho/src/main/kotlin/br/caos/models/UserRoles.kt
@@ -2,6 +2,10 @@ package br.caos.models
 
 import br.caos.view.RoleDto
 
+/** Model usado para a representação da entidade [UserRoles] do banco de dados.
+ * @param id [Int] Identificador da Entidade.
+ * @param roleName [String] Nome da função. Ex: professor.
+ */
 data class UserRoles (
     val id: Int,
     val roleName: String
--- fogo-no-parquinho/src/main/kotlin/br/caos/view/RoleDto.kt
+++ fogo-no-parquinho/src/main/kotlin/br/caos/view/RoleDto.kt
@@ -2,7 +2,7 @@ package br.caos.view
 
 import kotlinx.serialization.Serializable
 
-/** Data Object usado para comunicação e representação da entidade [Role] para a aplicação que consumirá a API.
+/** Data Object usado para comunicação e representação da entidade [UserRoles] para a aplicação que consumirá a API.
  * @param id [Int] Identificador da Entidade.
  * @param roleName [String] Nome da função. Ex: professor.
  */

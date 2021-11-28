bb42160c0c4bed8cb415f1e8135a3cffe481efae
julyca <jufsantana1@gmail.com>
2021-11-27 21:57:38 -0300

    Adicionando comentários ao arquivo de conexão com o banco de dados

--- fogo-no-parquinho/src/main/kotlin/br/caos/dao/ConnectionDAO.kt
+++ fogo-no-parquinho/src/main/kotlin/br/caos/dao/ConnectionDAO.kt
@@ -4,7 +4,7 @@ import br.caos.shared.SharedPaths
 import java.sql.*
 
 class ConnectionDAO {
-    var connection : Connection
+    private var connection : Connection
     var statement : Statement?
     var resultSet : ResultSet?
     var preparedStatement : PreparedStatement?
@@ -16,12 +16,19 @@ class ConnectionDAO {
         this.preparedStatement = null
     }
 
+    /** Método que executa uma query SQL.
+     * @param query [String] query SQL a ser executada
+     * @return resultado da query
+     */
     fun executeQuery(query : String) : ResultSet?{
         this.statement = this.connection.createStatement()
         this.resultSet = this.statement?.executeQuery(query)
         return this.resultSet
     }
 
+    /**
+     * Método que fecha todas as conexões abertas com o banco de dado
+     */
     fun close(){
         this.connection.close()
         this.statement?.close()
@@ -29,11 +36,18 @@ class ConnectionDAO {
         this.resultSet?.close()
     }
 
+    /** Método que prepara um [Statement] para uma determinada query
+     * @param query [String] query SQL
+     * @return o statement que deve ser utilizado.
+     */
     fun getPreparedStatement(query: String): PreparedStatement? {
         this.preparedStatement = this.connection?.prepareStatement(query)
         return this.preparedStatement
     }
 
+    /**
+     * Método que executa os comandos preparados sob uma conexão
+     */
     fun commit() {
         this.connection?.commit()
     }
